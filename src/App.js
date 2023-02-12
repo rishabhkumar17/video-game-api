adb3978f586788cc0955b2fdcd9b03531d17a114
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-12 23:17:54 +0530

    intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchSection">
+        <h1>Search For A Game</h1>
+        <input type="text" placeholder="Minecraft.." />
+        <button>Search Game Title</button>
+      </div>
+      <div className="dealsSection">
+        <h1>Latest Deals</h1>
+      </div>
     </div>
   );
 }

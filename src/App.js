38dd5ae204b3be7402cf60865b3ef4ad104f079e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-12 23:40:47 +0530

    search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [gameTitle, setGameTitle] = useState('');
+  const searchGame = () => {};
   return (
     <div className="App">
       <div className="searchSection">
         <h1>Search For A Game</h1>
-        <input type="text" placeholder="Minecraft.." />
+        <input
+          type="text"
+          placeholder="Minecraft.."
+          onChange={(e) => setGameTitle(e.target.value)}
+        />
         <button>Search Game Title</button>
       </div>
       <div className="dealsSection">

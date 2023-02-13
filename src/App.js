d1a15710ffc65a3b2be6ca002aec419baa41b8ee
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-13 05:59:08 +0530

    implementing SWR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 import './App.css';
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
 function App() {
   const [gameTitle, setGameTitle] = useState('');
   const [searchedGames, setSearchedGames] = useState([]);
-  const [gameDeals, setGameDeals] = useState([]);
-
+  // const [gameDeals, setGameDeals] = useState([]);
+  const { data, error } = useSWR(
+    'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=100&pageSize=3',
+    fetcher
+  );
   const searchGame = () => {
     fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
       .then((response) => response.json())
       .then((data) => setSearchedGames(data));
   };
 
-  useEffect(() => {
-    fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=100&pageSize=3
-    `)
-      .then((response) => response.json())
-      .then((data) => setGameDeals(data));
-  }, []);
   return (
     <div className="App">
       <div className="searchSection">
@@ -42,14 +42,15 @@ function App() {
       <div className="dealsSection">
         <h1>Latest Deals</h1>
         <div className="games">
-          {gameDeals.map((game, key) => (
-            <div className="game" id="deals" key={key}>
-              <h3>{game.title}</h3>
-              <p>Normal Price: {game.normalPrice}</p>
-              <p>Sale Price: {game.salePrice}</p>
-              <h3>YOU SAVE {game.savings.substr(0, 2)}%</h3>
-            </div>
-          ))}
+          {data &&
+            data.map((game, key) => (
+              <div className="game" id="deals" key={key}>
+                <h3>{game.title}</h3>
+                <p>Normal Price: {game.normalPrice}</p>
+                <p>Sale Price: {game.salePrice}</p>
+                <h3>YOU SAVE {game.savings.substr(0, 2)}%</h3>
+              </div>
+            ))}
         </div>
       </div>
     </div>
